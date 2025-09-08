26b34957163fd9695a59b304a527b9bf1b95a4f0
Tilroe <rowan@tilroe.com>
2025-09-07 23:46:15 -0600

    more geometry functions

--- src/geometry.h
+++ src/geometry.h
@@ -17,7 +17,39 @@ void vec4_normalize(vec4 in, vec4 out);
 float vec3_dot(const vec3 a, const vec3 b);
 float vec4_dot(const vec4 a, const vec4 b);
 
+void vec3_cross(vec3 a, vec3 b, vec3 c);
+
 // Matrix Operations
-void ident(mat4 out);
+
+/*
+* mat_copy: Copies src values into dest
+* 
+* @param[in] dest: destination matrix. Copies into this
+* @param[in] src: source matrix. Copies from this
+*/
+void mat_copy(mat4 dest, mat4 src);
+
+/*
+* Turn a matrix into an identity matrix
+* 
+* @param[in] mat: matrix to turn into the identity matrix
+*/
+void ident(mat4 mat);
+
+/*
+* mat_mul: Multiplies a by b (a on the left, b on the right). Stores the result in matrix c
+* 
+* @param[in] a: left matrix in multiplication
+* @param[in] b: right matrix in multiplication
+* @param[out] c: result of multiplication
+*/
+void mat_mul(mat4 a, mat4 b, mat4 c);
+
+/*
+* translate: Applies a translation to a given transformation matrix
+* 
+* @param[in] mat: transformation matrix to modify
+* @param[in] mat
+*/
 void translate(mat4 mat, const vec3 translation);
 #endif

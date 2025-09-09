26e0d5f58f95c800e568e8cfab84ba9aecd8c89b
Tilroe <rowan@tilroe.com>
2025-09-08 23:41:49 -0600

    finishing vector library

--- src/vector.c
+++ src/vector.c
@@ -1,11 +1,17 @@
 #include "vector.h"
 
-#include <math.h>
+#include <math.h> // for memcpy
 #include <string.h>
 
 static float TOLERANCE = 0.000000001f;
          
-// --------- VECTOR OPERATIONS ---------
+void vec3_copy(vec3 dest, const vec3 src) {
+    memcpy(dest, src, sizeof(float) * 3);
+}
+
+void vec4_copy(vec4 dest, const vec4 src) {
+    memcpy(dest, src, sizeof(float) * 4);
+}
 
 float vec3_norm(vec3 vec) {
     return sqrtf((vec[0] * vec[0]) + (vec[1] * vec[1]) + (vec[2] * vec[2]));
@@ -15,21 +21,21 @@ float vec4_norm(vec4 vec) {
     return sqrtf((vec[0] * vec[0]) + (vec[1] * vec[1]) + (vec[2] * vec[2]) + (vec[3] * vec[3]));
 }
 
-void vec3_normalize(vec3 in, vec3 out) {
-    float magnitude = vec3_norm(in);
+void vec3_normalize(vec3 vec) {
+    float magnitude = vec3_norm(vec);
     if (magnitude < TOLERANCE) return;
-    out[0] = in[0] / magnitude;
-    out[1] = in[1] / magnitude;
-    out[2] = in[2] / magnitude;
+    vec[0] /= magnitude;
+    vec[1] /= magnitude;
+    vec[2] /= magnitude;
 }
 
-void vec4_normalize(vec4 in, vec4 out) {
-    float magnitude = vec3_norm(in);
+void vec4_normalize(vec4 vec) {
+    float magnitude = vec4_norm(vec);
     if (magnitude < TOLERANCE) return;
-    out[0] = in[0] / magnitude;
-    out[1] = in[1] / magnitude;
-    out[2] = in[2] / magnitude;
-    out[3] = in[3] / magnitude;
+    vec[0] /= magnitude;
+    vec[1] /= magnitude;
+    vec[2] /= magnitude;
+    vec[3] /= magnitude;
 }
 
 float vec3_dot(vec3 a, vec3 b) {
@@ -40,7 +46,7 @@ float vec4_dot(vec4 a, vec4 b) {
     return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]) + (a[3] * b[3]);
 }
 
-void vec3_cross(vec3 a, vec3 b, vec3 c) {
+void vec3_cross(const vec3 a, const vec3 b, vec3 c) {
     c[0] = (a[1] * b[2]) - (a[2] * b[1]);
     c[1] = (a[2] * b[0]) - (a[0] * b[2]);
     c[2] = (a[0] * b[1]) - (a[1] * b[0]);
--- src/vector.h
+++ src/vector.h
@@ -4,15 +4,18 @@
 typedef float vec3[3];		// {x, y, z}
 typedef float vec4[4];		// {x, y, z, w}
 
+void vec3_copy(vec3 dest, const vec3 src);
+void vec4_copy(vec4 dest, const vec4 src);
+
 float vec3_norm(const vec3 vec);
 float vec4_norm(const vec4 vec);
 
-void vec3_normalize(vec3 in, vec3 out);
-void vec4_normalize(vec4 in, vec4 out);
+void vec3_normalize(vec3 vec);
+void vec4_normalize(vec4 vec);
 
 float vec3_dot(const vec3 a, const vec3 b);
 float vec4_dot(const vec4 a, const vec4 b);
 
-void vec3_cross(vec3 a, vec3 b, vec3 c);
+void vec3_cross(const vec3 a, const vec3 b, vec3 c);
 
 #endif

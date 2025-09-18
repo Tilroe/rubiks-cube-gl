39c79793f6b86fd6ee1c60f185d981175ba054e2
Tilroe <rowan@tilroe.com>
2025-09-17 22:11:56 -0600

    import fix

--- src/mouse_handler.h
+++ src/mouse_handler.h
@@ -1,7 +1,7 @@
 #ifndef MOUSE_HANDLER_H
 #define MOUSE_HANDLER_H
 
-#include "GLFW/glfw3.h"
+#include <GLFW/glfw3.h>
 
 void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
 void cursor_position_callback(GLFWwindow *window, double xpos_new, double ypos_new);

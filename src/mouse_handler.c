e5294b9cb85e51470cfa7630f269d2c0865023f1
Tilroe <rowan@tilroe.com>
2025-09-18 23:31:19 -0600

    raycasting on click

--- src/mouse_handler.c
+++ src/mouse_handler.c
@@ -3,17 +3,18 @@
 #include <stdbool.h>
 #include <math.h>
 
+#include "window.h"
 #include "camera.h"
 #include "cube.h"
 #include "quaternion.h"
-
-#include <stdio.h>
+#include "ray.h"
 
 #define M_PI acos(-1.0)
 
 static double xpos, ypos, xvel, yvel;
 static int mouse_state[2] = { GLFW_RELEASE, GLFW_RELEASE }; // {lmb, rmb}
 
+static ray calculate_ray();
 static void rotate_cube();
 static void rotate_face();
 
@@ -24,6 +25,9 @@ void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
 	if (mouse_state[GLFW_MOUSE_BUTTON_RIGHT] == GLFW_PRESS) {
 		rotate_cube();
 	}
+	else if (mouse_state[GLFW_MOUSE_BUTTON_LEFT] == GLFW_PRESS) {
+		ray r = calculate_ray();
+	}
 }
 
 void cursor_position_callback(GLFWwindow *window, double xpos_new, double ypos_new) {
@@ -32,6 +36,27 @@ void cursor_position_callback(GLFWwindow *window, double xpos_new, double ypos_n
 	xpos = xpos_new;
 	ypos = ypos_new;
 	if (mouse_state[GLFW_MOUSE_BUTTON_RIGHT] == GLFW_PRESS) rotate_cube();
+}
+
+// https://antongerdelan.net/opengl/raycasting.html
+
+static ray calculate_ray() {
+	float x = (2.0f * xpos) / window_width() - 1.0f;
+	float y = 1.0f - (2.0f * ypos) / window_height();
+	vec4 ray_eye, ray_world, ray_clip = { x, y, -1.0f, 1.0f };
+	mat4 proj, view;
+	get_perspective_matrix(60.f, (float)window_width() / (float)window_height(), 0.1f, 100.f, proj); mat_inverse(proj);
+	get_view_matrix(view); mat_inverse(view);
+	
+	mat_vec_mul(proj, ray_clip, ray_eye);
+	ray_eye[2] = -1.0f; ray_eye[3] = 0.0f;
+	mat_vec_mul(view, ray_eye, ray_world);
+	vec4_normalize(ray_world);
+
+	ray r;
+	vec3_copy(r.origin, *camera_pos());
+	vec3_copy(r.direction, ray_world);
+	return r;
 }
 
 static void rotate_cube() {
---
+++ src/ray.h
@@ -0,0 +1,11 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vector.h"
+
+typedef struct {
+	vec3 origin;
+	vec3 direction;
+} ray;
+
+#endif // !RAY_H

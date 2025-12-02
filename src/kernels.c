d281fb527d2360760610af672edd2bb2e37ea2d9
Tilroe <rowan@tilroe.com>
2025-12-01 22:12:44 -0700

    maintaining kernel objects / buffers between function calls

--- src/kernels.c
+++ src/kernels.c
@@ -3,6 +3,7 @@
 #define CL_TARGET_OPENCL_VERSION 200
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "CL/opencl.h"
 
@@ -17,7 +18,7 @@ static cl_context context;
 static cl_command_queue queue;
 
 void load_kernels();
-void vec_add_test();
+void vec_add_test(const int n_items, float in[n_items]);
 
 int opencl_init() {
     cl_int error_code_ret;
@@ -27,29 +28,33 @@ int opencl_init() {
     context = clCreateContext(NULL, 1, &device, NULL, NULL, &error_code_ret); if (!CL_CHECK(error_code_ret)) return 0;
     queue = clCreateCommandQueueWithProperties(context, device, NULL, &error_code_ret); if (!CL_CHECK(error_code_ret)) return 0;
 
-    vec_add_test();
+    vec_add_test(512, (float[512]){0.f});
+    vec_add_test(512, (float[512]){0.f});
 
     return 1;
 }
 
 
-void vec_add_test() {
-    int n_items = 512;
-    float in[512] = {0.f};
-    float out[512];
-
+void vec_add_test(const int n_items, float in[n_items]) {
+    static bool initialized = false;
+    static cl_kernel kernel;
+    static cl_mem buffer_in, buffer_out;
     cl_int error_code;
 
-    // Compile kernel
-    const char* program_source = read_file("../kernels/opencl/vec_add.cl");
-    cl_program program = clCreateProgramWithSource(context, 1, &program_source, NULL, &error_code);
-    free(program_source);
-    clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    cl_kernel kernel = clCreateKernel(program, "vec_add", &error_code);
+    if (!initialized) {
+        // Compile kernel
+        const char* program_source = read_file("../kernels/opencl/vec_add.cl");
+        const cl_program program = clCreateProgramWithSource(context, 1, &program_source, NULL, &error_code);
+        free(program_source);
+        clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+        kernel = clCreateKernel(program, "vec_add", &error_code);
 
-    // Create memory objects
-    cl_mem buffer_in = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * n_items, NULL, &error_code);
-    cl_mem buffer_out = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * n_items, NULL, &error_code);
+        // Create memory objects
+        buffer_in = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * n_items, NULL, &error_code);
+        buffer_out = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * n_items, NULL, &error_code);
+
+        initialized = true;
+    }
 
     // Copy data to input
     error_code = clEnqueueWriteBuffer(queue, buffer_in, CL_FALSE, 0, sizeof(cl_float) * n_items, in, 0, NULL, NULL);
@@ -58,12 +63,12 @@ void vec_add_test() {
     error_code = clSetKernelArg(kernel, 0, sizeof(buffer_in), &buffer_in);
     error_code = clSetKernelArg(kernel, 1, sizeof(buffer_out), &buffer_out);
     const cl_float c = 1.0f;
-    const cl_float4 v;
-    error_code = clSetKernelArg(kernel, 2, sizeof(cl_float), &c);
+    error_code = clSetKernelArg(kernel, 2, sizeof(float), &c);
     const size_t global_dimensions[] = {n_items, 0, 0};
     error_code = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_dimensions, NULL, 0, NULL, NULL);
 
     // Read back results
+    float out[n_items];
     error_code = clEnqueueReadBuffer(queue, buffer_out, CL_FALSE, 0, sizeof(cl_float) * n_items, out, 0, NULL, NULL);
 
     // Wait for everything to finish
